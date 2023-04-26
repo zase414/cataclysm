b1d507c1520e3a0c3074195e9b98c6dda669791e
zase414 <jakub.kecek@gmail.com>
2023-04-26 19:06:26 +0200

    first commit

--- src/main/java/main/KeyListener.java
+++ src/main/java/main/KeyListener.java
@@ -2,7 +2,7 @@ package main;
 import static org.lwjgl.glfw.GLFW.*;
 public class KeyListener {
     private static KeyListener instance;
-    private boolean keyPressed[] = new boolean[350];
+    private boolean[] keyPressed = new boolean[350];
 
     private KeyListener() {
 
