81b638aea881d86016dc6d972ab9d34a1c007716
zase414 <jakub.kecek@gmail.com>
2023-05-15 21:38:11 +0200

    vertex and element arrays now update

--- src/main/java/main/LevelEditorScene.java
+++ src/main/java/main/LevelEditorScene.java
@@ -3,6 +3,7 @@ package main;
 import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
 import render.Shader;
+import util.Time;
 
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
@@ -13,33 +14,33 @@ import static org.lwjgl.opengl.GL20.*;
 public class LevelEditorScene extends Scene{
     private int vertexID, fragmentID, shaderProgram;
 
-    private float[] vertexArray = {
+    public float[] vertexArray = {
         // position              // color
-         50f, -50f, 0.0f,       1.0f, 0.0f, 0.0f, 1.0f,   // bottom right  0
-        -50f,  50f, 0.0f,       1.0f, 1.0f, 0.0f, 1.0f,   // top left      1
-         50f,  50f, 0.0f,       0.0f, 0.0f, 1.0f, 1.0f,   // top right     2
-        -50f, -50f, 0.0f,       1.0f, 0.0f, 1.0f, 1.0f    // bottom left   3
+         100.0f,  100.0f, 0.0f,     1.0f, 0.0f, 0.0f, 0.0f,
+         100.0f, -100.0f, 0.0f,     1.0f, 1.0f, 0.0f, 0.0f,
+        -100.0f,  100.0f, 0.0f,     1.0f, 1.0f, 1.0f, 0.0f,
+        -100.0f, -100.0f, 0.0f,     1.0f, 0.0f, 0.5f, 0.0f
     };
 
 
     // ONLY COUNTER-CLOCKWISE ORDER WORKS
     private int[] elementArray = {
-        2,1,0,  // top right triangle
-        0,1,3   // bottom left triangle
+        1,0,2,  // top right triangle
+        0,2,3   // bottom left triangle
     };
 
     private int vaoID, vboID, eboID;
     private Shader defaultShader;
 
     public LevelEditorScene() {
-
     }
     @Override
     public void init() {
+        // initialize the camera
         this.camera = new Camera(new Vector2f());
+        // import and compile the shaders
         defaultShader = new Shader("assets/shaders/default.glsl");
         defaultShader.compile();
-
 
         // ---------------------------------
         // generate VAO, VBO, EBO and send them to GPU
@@ -55,7 +56,7 @@ public class LevelEditorScene extends Scene{
         // create VBO upload vertex buffer
         vboID = glGenBuffers();
         glBindBuffer(GL_ARRAY_BUFFER, vboID);
-        glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_DYNAMIC_DRAW);
 
         // create the indices and upload
         IntBuffer elementBuffer = BufferUtils.createIntBuffer(elementArray.length);
@@ -63,43 +64,64 @@ public class LevelEditorScene extends Scene{
 
         eboID = glGenBuffers();
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer, GL_STATIC_DRAW);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer, GL_DYNAMIC_DRAW);
 
         // add vertex attribute pointers
         int positionsSize = 3;
         int colorSize = 4;
         int floatSizeBytes = 4;
         int vertexSizeBytes = (positionsSize + colorSize) * floatSizeBytes;
+
         // position attributes
         glVertexAttribPointer(0, positionsSize, GL_FLOAT, false, vertexSizeBytes, 0);
         glEnableVertexAttribArray(0);
+
         // color attributes
         glVertexAttribPointer(1, colorSize, GL_FLOAT, false, vertexSizeBytes, positionsSize * floatSizeBytes);
         glEnableVertexAttribArray(1);
     }
     @Override
     public void update(float dt) {
-        camera.position.x -= dt * 50.0f;
-        camera.position.y -= dt * 50.0f;
 
+        camera.position.x += 2*MouseListener.getDX();
+        camera.position.y += -2*MouseListener.getDY();
+
+        vertexArray[0] += MouseListener.getDX();
+        vertexArray[1] += MouseListener.getDY();
+
+
+        // update the vertex array
+        FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(vertexArray.length);
+        vertexBuffer.put(vertexArray).flip();
+        glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_DYNAMIC_DRAW);
+
+        // update the element array
+        IntBuffer elementBuffer = BufferUtils.createIntBuffer(elementArray.length);
+        elementBuffer.put(elementArray).flip();
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer, GL_DYNAMIC_DRAW);
+
+        // use the shader and upload values
         defaultShader.use();
 
         defaultShader.uploadMat4f("uProjection", camera.getProjectionMatrix());
         defaultShader.uploadMat4f("uView", camera.getViewMatrix());
-        // Bind the VAO
+        defaultShader.uploadFloat("uTime", Time.getTime());
+
+        // bind the VAO
         glBindVertexArray(vaoID);
 
         // enable the vertex attribute pointers
         glEnableVertexAttribArray(0);
         glEnableVertexAttribArray(1);
 
+        // draw
         glDrawElements(GL_TRIANGLES, elementArray.length, GL_UNSIGNED_INT, 0);
 
         // unbind everything
         glDisableVertexAttribArray(0);
         glDisableVertexAttribArray(1);
-
         glBindVertexArray(0);
         defaultShader.detach();
+        MouseListener.endFrame();
     }
 }
