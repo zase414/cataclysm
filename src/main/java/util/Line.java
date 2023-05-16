459e60496e3eaa630c87c5e1073c797e51b9f42b
zase414 <jakub.kecek@gmail.com>
2023-05-16 22:06:46 +0200

    merge IntersectionTest with Line

--- src/main/java/util/Line.java
+++ src/main/java/util/Line.java
@@ -42,7 +42,38 @@ public class Line {
         return t >= tMin && t <= tMax && t2 >= tMin && t2 <= tMax;
     }
 
+    public static void main(String[] args) {
 
 
+        // TEST
+
+        Line line1 = new Line();
+        Line line2 = new Line();
+
+        line1.x1 = -2.0f;
+        line1.y1 = 1.0f;
+        line1.x2 = 0.0f;
+        line1.y2 = 4.0f;
+
+        line2.x1 = -2.0f;
+        line2.y1 = 2.5f;
+        line2.x2 = 0.0f;
+        line2.y2 = 3.0f;
+
+        line1.init();
+        line2.init();
+
+        System.out.println("1dx: " + line1.dx);
+        System.out.println("1dy: " + line1.dy);
+
+        System.out.println("2dx: " + line2.dx);
+        System.out.println("2dy: " + line2.dy);
+
+
+        System.out.println(Line.areIntersecting(line1,line2));
+        System.out.println(Line.getIntersection(line1,line2).x + " // " + Line.getIntersection(line1, line2).y);
+
+
+    }
 
 }
