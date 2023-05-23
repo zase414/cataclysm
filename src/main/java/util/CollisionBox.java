aef1e05928c9b63e5f3ec10981afff00629d9c8c
zase414 <jakub.kecek@gmail.com>
2023-05-23 19:44:56 +0200

    Line class is abstract, Ray and Wall extend it

--- src/main/java/util/CollisionBox.java
+++ src/main/java/util/CollisionBox.java
@@ -4,7 +4,7 @@ import main.Map;
 import main.Player;
 import org.joml.Vector2f;
 public class CollisionBox {
-    public Line[] bounds = new Line[4];
+    public Ray[] bounds = new Ray[4];
     public float size;
     public void update(Player player) {
         float posX = player.posX;
@@ -13,17 +13,17 @@ public class CollisionBox {
         Vector2f tr = new Vector2f(posX + size, posY + size); // top right
         Vector2f bl = new Vector2f(posX - size, posY - size); // bottom left
         Vector2f br = new Vector2f(posX + size, posY - size); // bottom right
-        bounds[0] = new Line(tl.x, tl.y, tr.x, tr.y);
-        bounds[1] = new Line(tr.x, tr.y, br.x, br.y);
-        bounds[2] = new Line(br.x, br.y, bl.x, bl.y);
-        bounds[3] = new Line(bl.x, bl.y, tl.x, tl.y);
+        bounds[0] = new Ray(tl.x, tl.y, tr.x, tr.y);
+        bounds[1] = new Ray(tr.x, tr.y, br.x, br.y);
+        bounds[2] = new Ray(br.x, br.y, bl.x, bl.y);
+        bounds[3] = new Ray(bl.x, bl.y, tl.x, tl.y);
     }
 
     public boolean checkForCollisionsPlayer(Map map, Player player) {
         boolean isClipping = false;
         for (Line bound:player.collisionBox.bounds) {
-            for (Line wall:map.walls) {
-                if (Line.areIntersecting(bound, wall)) {
+            for (Wall wall:map.walls) {
+                if (Ray.areIntersecting(bound, wall)) {
                     isClipping = true;
                 }
             }
