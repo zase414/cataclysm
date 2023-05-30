b92ef08373684abff32cae7576e0dd1770aca8ad
zase414 <jakub.kecek@gmail.com>
2023-05-30 20:01:29 +0200

    update the way the nearest wall is found, add methods from elsewhere for clarity

--- src/main/java/render/RayCaster.java
+++ src/main/java/render/RayCaster.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import main.Map;
+import main.MouseListener;
 import main.Player;
 import org.joml.Vector2f;
 import util.*;
@@ -46,40 +47,85 @@ public class RayCaster {
             rays.add(ray);
         }
         for (Ray ray:rays) {
-            float minDistance = renderDistance + 1;
+            float minDist = renderDistance + 1;
+            Wall nearestXWall = new Wall();
+            Vector2f nearestXCoordinates = new Vector2f();
+            float nearestXT = 0;
             for (Wall wall: map.walls) {
                 if (Ray.areIntersecting(ray,wall)) {
-                    // calculate the distance of the intersection to the player
-                    Vector2f intersection = Ray.getIntersection(ray,wall);
-                    float intersectionT = Ray.getIntersectionT(ray, wall);
                     ray.intersectedAnything = true;
 
+                    Vector2f intersection = Ray.getIntersection(ray,wall);
+                    float intersectionT = Ray.getIntersectionT(wall, ray);
+
+                    // calculate the distance of the intersection to the player
                     double dx = player.posX - intersection.x;
                     double dy = player.posY - intersection.y;
                     double wallDistance = Math.sqrt((dx * dx) + (dy * dy));
 
-                    if (wallDistance < minDistance) {
-                        minDistance = (float) wallDistance;
-                        ray.r = Math.max(Math.min(wall.r - wall.r * (minDistance / fadeOutDistance), wall.r), wall.r / 100.0f);
-                        ray.g = Math.max(Math.min(wall.g - wall.g * (minDistance / fadeOutDistance), wall.g), wall.g / 100.0f);
-                        ray.b = Math.max(Math.min(wall.b - wall.b * (minDistance / fadeOutDistance), wall.b), wall.b / 100.0f);
-                        ray.a = wall.a;
-                        ray.firstIntersection = intersection;
-                        ray.intersectedWall = wall;
-                        ray.intersectionT = intersectionT;
+                    // get the values of the nearest wall
+                    if (wallDistance < minDist) {
+                        minDist = (float) wallDistance;
+                        nearestXWall = wall;
+                        nearestXCoordinates = intersection;
+                        nearestXT = intersectionT;
                     }
-
-
-                    // ==== debug ====
-                    //System.out.println("intersection: " + intersection.x + " ; " + intersection.y);
                 }
             }
-            if (minDistance != 0.0 && minDistance < renderDistance + 1) {
-                ray.distanceToWall = minDistance;
-
+            if (ray.intersectedAnything) {
+                ray.distanceToWall = minDist;
+                float r = nearestXWall.r, g = nearestXWall.g, b = nearestXWall.b, a = nearestXWall.a;
+                ray.r = Math.max(Math.min(r - r * (minDist / fadeOutDistance), r), r / 100.0f);
+                ray.g = Math.max(Math.min(g - g * (minDist / fadeOutDistance), g), g / 100.0f);
+                ray.b = Math.max(Math.min(b - b * (minDist / fadeOutDistance), b), b / 100.0f);
+                ray.a = a;
+                ray.firstIntersection = nearestXCoordinates;
+                ray.intersectedWall = nearestXWall;
+                ray.intersectionT = nearestXT;
             }
-            //System.out.println(ray.distanceToWall);
         }
         this.rays = rays;
+    }
+
+    public void updateMapVisibility() {
+        List<List<Ray>> chainList = divideRays(rays);
+
+        for (List<Ray> sameWallRays : chainList) {
+            Ray startRay = sameWallRays.get(0);
+            Ray endRay = sameWallRays.get(sameWallRays.size() - 1);
+            float minT = Math.min(startRay.intersectionT, endRay.intersectionT);
+            float maxT = Math.max(startRay.intersectionT, endRay.intersectionT);
+
+            if (minT < startRay.intersectedWall.minVisibleT) {
+                startRay.intersectedWall.minVisibleT = minT;
+            }
+            if (maxT > startRay.intersectedWall.maxVisibleT) {
+                startRay.intersectedWall.maxVisibleT = maxT;
+            }
+        }
+    }
+
+    public static List<List<Ray>> divideRays(List<Ray> rays) {
+        // divides rays into sublists based on the wall they intersected
+        // i.e. example wall ids: 111 222 11 22 333
+        List<List<Ray>> sublists = new ArrayList<>();
+        List<Ray> currentSublist = new ArrayList<>();
+        int currentID = -1;
+        for (Ray ray : rays) {
+            int wallID = ray.intersectedWall.id;
+            if (!ray.intersectedAnything) {
+                continue;
+            }
+            if (wallID != currentID) {
+                currentSublist = new ArrayList<>();
+                sublists.add(currentSublist);
+            }
+            currentSublist.add(ray);
+            currentID = wallID;
+        }
+        return sublists;
+    }
+    public void adjustRayCount() {
+        rayCount = Math.max(rayCount + (int) MouseListener.getScrollY(), 5);
     }
 }
