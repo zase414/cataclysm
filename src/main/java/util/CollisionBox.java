4b0d1b506b3b3dd9b971e05fe678f9b43ca0c8bc
zase414 <jakub.kecek@gmail.com>
2023-06-08 21:43:50 +0200

    move collision checking to Player class

--- src/main/java/util/CollisionBox.java
+++ src/main/java/util/CollisionBox.java
@@ -1,19 +1,6 @@
 package util;
 
-import main.Map;
-import main.Player;
 public class CollisionBox {
     public Ray[] bounds = new Ray[4];
     public float size;
-    public static boolean checkForCollisionsPlayer(Map map, Player player) {
-        boolean isClipping = false;
-        for (Line bound:player.collisionBox.bounds) {
-            for (Wall wall:map.walls) {
-                if (Ray.areIntersecting(bound, wall)) {
-                    isClipping = true;
-                }
-            }
-        }
-        return isClipping;
-    }
 }
