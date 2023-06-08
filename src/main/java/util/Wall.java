af7458de18c140007e731ad862c5f3b8791266c8
zase414 <jakub.kecek@gmail.com>
2023-06-08 21:38:43 +0200

    add height parameters to walls

--- src/main/java/util/Wall.java
+++ src/main/java/util/Wall.java
@@ -3,7 +3,7 @@ package util;
 public class Wall extends Line {
     public Color color = new Color();
     public int id;
-    public float height;
+    public float topHeight, botHeight;
     public Wall(float x1, float y1, float x2, float y2) {
         super(x1, y1, x2, y2);
     }
