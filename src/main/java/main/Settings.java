ee554b78fc582f6b16aac2e7280f9a9da5d2d6e9
zase414 <118931761+zase414@users.noreply.github.com>
2023-12-12 07:15:39 +0100

    remove testing package

--- src/main/java/main/Settings.java
+++ src/main/java/main/Settings.java
@@ -9,14 +9,9 @@ public class Settings {
     public static float fadeOutDistance = 50.0f;
     static float fov = 80.0f;
     static int rayCount = 600;
-<<<<<<< HEAD
     static boolean mapFullyVisible = false;
     static boolean cameraFollowsPlayer = true;
-=======
-    static boolean mapWallsFullyVisible = false;
-    static boolean mapCameraFollowsPlayer = true;
 
     // movement
->>>>>>> parent of 1599778 (screen resolution moved to Settings class, other minor changes)
     static boolean flappyBird = false;
 }
